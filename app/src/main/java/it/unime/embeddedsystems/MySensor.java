1ce7cc5c518506e7fd04de47b5f3d349ac1c7486
Luca D'Amico <damico.luca91@live.it>
2016-05-19 00:12:25 +0200

    Updated MySensor

--- app/src/main/java/it/unime/embeddedsystems/MySensor.java
+++ app/src/main/java/it/unime/embeddedsystems/MySensor.java
@@ -11,57 +11,49 @@ import java.util.List;
 /**
  * Created by andfa on 18/05/2016.
  */
-class MySensor implements SensorEventListener {
+class MySensor {
 
     Context mContext;
 
     SensorManager sensorManager;
-    Sensor sensor;
+    Sensor tempSensor, pressureSensor, crepuscolarSensor;
+    SensorEventListener tempSensorListener, pressureSensorListener, crepuscolarSensorListener;
 
-    List<Sensor> mSensorList;
+    float currentTemp;
 
-    public MySensor(Context context, List<Sensor> sensorList) {
+    public MySensor(Context context) {
         mContext = context;
-        mSensorList = sensorList;
+
+        sensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+
+        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        registerTempSensor(tempSensor);
+
+        // Inserisci qui pressure e crepuscolar
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
+    void registerTempSensor(Sensor sensor){
+        tempSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                currentTemp = event.values[0];
+            }
 
-        for(int k=0; k<mSensorList.size(); k++){
-
-            switch (mSensorList.get(k).getType()){
-
-                case Sensor.TYPE_PRESSURE:
-                    break;
-
-                default:
-                    break;
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
             }
-        }
+        };
 
+        sensorManager.registerListener(tempSensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override
-    public void onAccuracyChanged(android.hardware.Sensor sensor, int accuracy) {
-
-    }
-
-    public Sensor initializeSensor(int sensorType){
-        sensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(sensorType);
-        sensorManager.registerListener(this, sensor , SensorManager.SENSOR_DELAY_NORMAL);
-
-        return sensor;
-
-    }
-
-    public void registerSensorListener(){
-        sensorManager.registerListener(this, sensor , SensorManager.SENSOR_DELAY_NORMAL);
+    public float getCurrentTemp(){
+        return currentTemp;
     }
 
     public void unregisterSensorListener(){
-        sensorManager.unregisterListener(this);
+        sensorManager.unregisterListener(tempSensorListener);
+        // RIMUOVI QUI pressureSensorListener e crepuscolarSensorListener
     }
 }
