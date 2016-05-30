4ef42c67833d7119bb5e5157595b181cae2e132d
Luca D'Amico <damico.luca91@live.it>
2016-05-30 18:51:12 +0200

    Updated MainActivity

--- app/src/main/java/it/unime/embeddedsystems/MainActivity.java
+++ app/src/main/java/it/unime/embeddedsystems/MainActivity.java
@@ -1,31 +1,23 @@
 package it.unime.embeddedsystems;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Sensor tempSensor;
     private TextView temperatureTXV;
     private String serverUrl = "http://smartme-data.unime.it/api/3/action/datastore_upsert";
-
-
+    private SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,30 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
         temperatureTXV.setText("Latitudine: "+gps.getLatitude()+ "  Logitudine: "+gps.getLongitude());
 
-     /*   sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        tempSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);*/
 
-      /*  RestAdapter adapter = new RestAdapter.Builder().setEndpoint(url).build();
+        boolean isDeviceRegistered = sharedPref.getBoolean("isDeviceRegistered",false);
 
-        RestInterface restInterface = adapter.create(RestInterface.class);
-
-        //Calling method to get whether report
-        restInterface.getWheatherReport(new Callback<Model>() {
-            @Override
-            public void success(Model model, Response response) {
-                System.out.println(model.getName());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-
-                String merror = error.getMessage();
-                System.out.println(merror);
-            }
-        });*/
-
-        String command = "{\"resource_id\":\"c35b761d-8f4a-4b89-a68e-fcdb8063b636\", \"method\":\"insert\", \"records\":[{\"Type\":\"TYPE_ACCELEROMETER\",\"Model\":\"Accelerometer\",\"Unit\":\"m/s^2\",\"FabricName\":\"-\",\"ResourceID\":\"71ae4c3c-3f2b-4c31-ba09-1d83444327d2\",\"Date\":\"2016-05-13T12:00:00\"}]}";
-        //new Task().execute(serverUrl, authorization, command);
+        if(!isDeviceRegistered){
+            new RegisterDevice().execute();
+        }
     }
 
     @Override
@@ -138,10 +111,24 @@ public class MainActivity extends AppCompatActivity {
             return datasetUUID;
         }
 
-        String createDatastore(String datasetName, String datastoreName){
+        String createSensorDatastore(String datasetName, String datastoreName, String modelName){
             String datastoreUUID = "";
             try {
-                String command = "{\"resource\": {\"package_id\":\""+datasetName+"\", \"name\":\""+datastoreName+"\"}, \"fields\": [ {\"id\": \"Type\", \"type\":\"text\"}, {\"id\": \"Model\", \"type\":\"text\"}, {\"id\": \"Unit\", \"type\":\"text\"}, {\"id\": \"FabricName\", \"type\":\"text\"}, {\"id\": \"ResourceID\", \"type\":\"text\"}, {\"id\": \"Date\", \"type\":\"timestamp\"}] }";
+                String command = "{\"resource\": {\"package_id\":\""+datasetName+"\", \"name\":\""+datastoreName+"\"}, \"fields\": [ {\"id\": \"Type\", \"type\":\"text\"}, {\"id\": "+modelName+", \"type\":\"text\"}, {\"id\": \"Unit\", \"type\":\"text\"}, {\"id\": \"FabricName\", \"type\":\"text\"}, {\"id\": \"ResourceID\", \"type\":\"text\"}, {\"id\": \"Date\", \"type\":\"timestamp\"}] }";
+                JSONObject response = POST("http://smartme-data.unime.it/api/3/action/datastore_create", "22c5cfa7-9dea-4dd9-9f9d-eedf296852ae", command); // Create Datastore
+                JSONObject result = new JSONObject(response.getString("result"));
+                datastoreUUID = result.getString("resource_id");
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            return datastoreUUID;
+        }
+
+        String createDatastore(String datasetName, String datastoreName, String modelName){
+            String datastoreUUID = "";
+            try {
+                String command = "{\"resource\": {\"package_id\":\""+datasetName+"\", \"name\":\""+datastoreName+"\"}, \"fields\": [ {\"id\": \"Date\", \"type\":\"timestamp\"}, {\"id\": \""+modelName+"\", \"type\":\"text\"}, {\"id\": \"Altitude\", \"type\":\"numeric\"}, {\"id\": \"Latitude\", \"type\":\"numeric\"}, {\"id\": \"Longitude\", \"type\":\"numeric\"}] }";
+
                 JSONObject response = POST("http://smartme-data.unime.it/api/3/action/datastore_create", "22c5cfa7-9dea-4dd9-9f9d-eedf296852ae", command); // Create Datastore
                 JSONObject result = new JSONObject(response.getString("result"));
                 datastoreUUID = result.getString("resource_id");
@@ -153,21 +140,21 @@ public class MainActivity extends AppCompatActivity {
 
         void insertInSensorDatastore(String sensorUUID, String measureUUID, String type, String name, String unit){
             String command = "{\"resource_id\":\""+sensorUUID+"\", \"method\":\"insert\", \"records\":[{\"Type\":\""+type+"\",\"Model\":\""+name+"\",\"Unit\":\""+unit+"\",\"FabricName\":\"-\",\"ResourceID\":\""+measureUUID+"\",\"Date\":\"2016-05-13T12:00:00\"}]}";
-            POST("http://smartme-data.unime.it/api/3/action/datastore_upsert", "22c5cfa7-9dea-4dd9-9f9d-eedf296852ae", command); // Create Datastore
+            POST("http://smartme-data.unime.it/api/3/action/datastore_upsert", "22c5cfa7-9dea-4dd9-9f9d-eedf296852ae", command); // Insert in Datastore
         }
 
         @Override
         protected JSONObject doInBackground(String... params) {
             String datasetUUID = createDataset(params[0]);
-            String sensorsDatastoreUUID = createDatastore("","sensors");
+            String sensorsDatastoreUUID = createSensorDatastore("","sensors","Model");
 
-            String temperatureDatastoreUUID = createDatastore("","temperature");
+            String temperatureDatastoreUUID = createDatastore("","temperature","Temperature");
             insertInSensorDatastore(sensorsDatastoreUUID,temperatureDatastoreUUID, "TYPE_TEMPERATURE", "TEMPERATURE", "celsius");
 
-            String pressureDatastoreUUID = createDatastore("","pressure");
+            String pressureDatastoreUUID = createDatastore("","pressure", "Pressure");
             insertInSensorDatastore(sensorsDatastoreUUID,pressureDatastoreUUID, "TYPE_PRESSURE", "PRESSURE", "mbar");
 
-            String lightDatastoreUUID = createDatastore("","light");
+            String lightDatastoreUUID = createDatastore("","light", "Light");
             insertInSensorDatastore(sensorsDatastoreUUID,lightDatastoreUUID, "TYPE_LIGHT", "LIGHT", "lx");
 
             JSONObject data = null;
@@ -186,9 +173,50 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(JSONObject data) {
-
+            try {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("isDeviceRegistered", true);
+                editor.putString("datasetUUID", data.getString("datasetUUID"));
+                editor.putString("sensorsDatastoreUUID", data.getString("sensorsDatastoreUUID"));
+                editor.putString("temperatureDatastoreUUID", data.getString("temperatureDatastoreUUID"));
+                editor.putString("pressureDatastoreUUID", data.getString("pressureDatastoreUUID"));
+                editor.putString("lightDatastoreUUID", data.getString("lightDatastoreUUID"));
+                editor.commit();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
     }
+
+
+
+    private class SendData extends AsyncTask<String, Void, Void> {
+
+        void insertInDatastore(String measureUUID, String sensorName, String sensorValue, String lat, String lon, String date){
+            String command = "{\"resource_id\":\""+measureUUID+"\", \"method\":\"insert\", \"records\":[{\"Latitude\":\""+lat+"\",\"Altitude\":\"0\",\"Date\":\""+date+"\",\""+sensorName+"\":\""+sensorValue+"\",\"Longitude\":\"+"lon"+\"}]}";
+            POST("http://smartme-data.unime.it/api/3/action/datastore_upsert", "22c5cfa7-9dea-4dd9-9f9d-eedf296852ae", command); // Insert in Datastore
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String measureUUID = params[0];
+            String sensorName = params[1];
+            String sensorValue = params[2];
+            String lat = params[3];
+            String lon = params[4];
+            String date = params[5];
+            insertInDatastore(measureUUID, sensorName, sensorValue, lat, lon, date);
+            return null;
+        }
+    }
+
+
+
+
+
+
+
+
 
 }
